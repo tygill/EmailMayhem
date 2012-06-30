3c5ce14e7c237db4fcbdb54cf5947b207aba0f13
Tyler Gill <tyler.gill@byu.edu>
2012-06-29 21:38:23 -0700

    Email receive event is now working, and has some nifty little POP helpers built in for gmail and hotmail.

---
+++ EmailMayhem/EmailReceiveEvent.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading;
+using System.Text;
+
+using MayhemCore;
+using MayhemWpf.ModuleTypes;
+using MayhemWpf.UserControls;
+
+using OpenPop.Mime.Header;
+using OpenPop.Pop3;
+
+namespace EmailMayhem {
+
+    [DataContract]
+    [MayhemModule("Email Received", "Triggers when an email is received")]
+    public class EmailReceiveEvent : EventBase, IWpfConfigurable {
+
+        [DataMember]
+        private string Hostname { get; set; }
+
+        [DataMember]
+        private int Port { get; set; }
+
+        [DataMember]
+        private bool UseSsl { get; set; }
+
+        [DataMember]
+        private string EmailAddress { get; set; }
+
+        [DataMember]
+        private string Password { get; set; }
+
+        [DataMember]
+        private string Subject { get; set; }
+
+        [DataMember]
+        private string From { get; set; }
+
+        private bool Done;
+
+        private Thread PollingThread;
+
+        private object Lock;
+
+        protected override void OnLoadDefaults() {
+            base.OnLoadDefaults();
+
+            Hostname = String.Empty;
+            Port = 995;
+            UseSsl = true;
+            EmailAddress = String.Empty;
+            Password = String.Empty;
+            Subject = String.Empty;
+            From = String.Empty;
+        }
+
+        protected override void OnLoadFromSaved() {
+            base.OnLoadFromSaved();
+        }
+
+        protected override void OnAfterLoad() {
+            base.OnAfterLoad();
+            Lock = new object();
+            Done = false;
+        }
+
+        protected override void OnDeleted() {
+            base.OnDeleted();
+
+            lock (Lock) {
+                Done = true;
+            }
+            if (PollingThread != null) {
+                PollingThread.Join();
+                PollingThread = null;
+            }
+        }
+
+        protected override void OnEnabling(EnablingEventArgs e) {
+            base.OnEnabling(e);
+            lock (Lock) {
+                Done = false;
+            }
+
+            // Start up the polling thread
+            PollingThread = new Thread(EmailReceiveThread);
+            PollingThread.Start();
+        }
+
+        protected override void OnDisabled(DisabledEventArgs e) {
+            base.OnDisabled(e);
+            lock (Lock) {
+                Done = true;
+            }
+            if (PollingThread != null) {
+                PollingThread.Join();
+                PollingThread = null;
+            }
+        }
+
+        public string GetConfigString() {
+            lock (Lock) {
+                return EmailAddress;
+            }
+        }
+
+        public WpfConfiguration ConfigurationControl {
+            get {
+                EmailReceiveEventConfig config;
+                lock (Lock) {
+                    config = new EmailReceiveEventConfig(Hostname, Port, UseSsl, EmailAddress, Password, Subject, From);
+                }
+                return config;
+            }
+        }
+
+        public void OnSaved(WpfConfiguration configurationControl) {
+            EmailReceiveEventConfig config = configurationControl as EmailReceiveEventConfig;
+            lock (Lock) {
+                Hostname = config.Hostname;
+                Port = config.Port;
+                UseSsl = config.UseSsl;
+                EmailAddress = config.EmailAddress;
+                Password = config.Password;
+                Subject = config.Subject.Trim();
+                From = config.From.Trim();
+            }
+        }
+
+        private Pop3Client InitiateClient() {
+            Pop3Client client = new Pop3Client();
+            try {
+                client.Connect(Hostname, Port, UseSsl);
+                client.Authenticate(EmailAddress, Password);
+            } catch {
+                // Ignore exceptions - let them be raised in the thread
+                // There's not really a good way to debug this anyway if something goes wrong at this point.
+            }
+            return client;
+        }
+
+        private void EmailReceiveThread() {
+
+            Pop3Client client = null;
+            int messageCount = 0;
+            using (client = InitiateClient()) {
+                messageCount = client.GetMessageCount();
+            }
+            
+            int pollingDelay = 15000;
+
+            bool done = false;
+            while (!done) {
+
+                lock (Lock) {
+                    done = Done;
+
+                    using (client = InitiateClient()) {
+                        int newMessageCount = client.GetMessageCount();
+                        // Try to get each message between the previous latest message and the current latest message
+                        // If we find a message thats valid and new, we trigger
+                        bool trigger = false;
+                        for (int message = messageCount + 1; message <= newMessageCount; message++) {
+                            // The message might be deleted, so a try catch is needed
+                            try {
+                                MessageHeader header = client.GetMessageHeaders(message);
+
+                                // Match the subject
+                                bool subjectPasses = false;
+                                if (!string.IsNullOrEmpty(Subject)) {
+                                    if (header.Subject.ToLower().Contains(Subject.ToLower())) {
+                                        subjectPasses = true;
+                                    }
+                                } else {
+                                    subjectPasses = true;
+                                }
+
+                                // Match the sender
+                                bool fromPasses = false;
+                                if (!string.IsNullOrEmpty(From)) {
+                                    // Compare against the email address
+                                    if (!string.IsNullOrEmpty(header.Sender.Address)) {
+                                        if (header.Sender.Address.ToLower().Contains(From.ToLower())) {
+                                            fromPasses = true;
+                                        }
+                                    // Then try to compare against the display name
+                                    } else if (!string.IsNullOrEmpty(header.Sender.DisplayName)) {
+                                        if (header.Sender.DisplayName.ToLower().Contains(From.ToLower())) {
+                                            fromPasses = true;
+                                        }
+                                    }
+                                    // If neither of those existed, then who knows who this is from, but it isn't one of the people you asked for
+                                } else {
+                                    fromPasses = true;
+                                }
+
+                                // If we've either already triggered from some other message, or if this message's subject and from checks passes, then we need to trigger as soon as we finish
+                                trigger = trigger || (subjectPasses && fromPasses);
+                            } catch {
+                                // Just move along to the next message
+                            }
+                        }
+
+                        if (trigger) {
+                            Trigger();
+                        }
+
+                        messageCount = newMessageCount;
+                    }
+                }
+
+                if (!done) {
+                    // Sleep till the next time we should check things
+                    Thread.Sleep(pollingDelay);
+                }
+            }
+        }
+    }
+}
---
+++ EmailMayhem/EmailReceiveEventConfig.xaml.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using MayhemCore;
+using MayhemWpf.UserControls;
+
+using OpenPop.Pop3;
+
+namespace EmailMayhem {
+    /// <summary>
+    /// Interaction logic for EmailReceiveEventConfig.xaml
+    /// </summary>
+    public partial class EmailReceiveEventConfig : WpfConfiguration {
+
+        public EmailReceiveEventConfig(string hostname, int port, bool useSsl, string emailAddress, string password, string subject, string from) {
+            InitializeComponent();
+
+            Hostname = hostname;
+            Port = port;
+            UseSsl = useSsl;
+            EmailAddress = emailAddress;
+            Password = password;
+            Subject = subject;
+            From = from;
+        }
+
+        public string Hostname { get; private set; }
+
+        public int Port { get; private set; }
+
+        public bool UseSsl { get; private set; }
+
+        public string EmailAddress { get; private set; }
+
+        public string Password { get; private set; }
+
+        public string Subject { get; private set; }
+
+        public string From { get; private set; }
+
+        public override void OnLoad() {
+            base.OnLoad();
+
+            hostname.Text = Hostname;
+            port.Text = Port != 0 ? Port.ToString() : String.Empty;
+            useSsl.IsChecked = UseSsl;
+            emailAddress.Text = EmailAddress;
+            password.Password = Password;
+            subject.Text = Subject;
+            from.Text = From;
+
+            Validate();
+        }
+
+        public override string Title {
+            get {
+                return "Receive Email";
+            }
+        }
+
+        private void Unvalidate() {
+            CanSave = false;
+            checkButton.IsEnabled = true;
+        }
+
+        private void Validate() {
+            // If everything is valid and we can make a connection, then set CanSave to true
+            try {
+                Hostname = hostname.Text;
+                Port = int.Parse(port.Text);
+                UseSsl = useSsl.IsChecked.GetValueOrDefault();
+                EmailAddress = emailAddress.Text;
+                // This is a fun line to have...I almost feel like I should send it in to the daily wtf
+                Password = password.Password;
+                Subject = subject.Text;
+                From = from.Text;
+
+                using (Pop3Client client = new Pop3Client()) {
+                    client.Connect(Hostname, Port, UseSsl);
+
+                    if (!client.Connected) {
+                        CanSave = false;
+                        return;
+                    }
+
+                    client.Authenticate(EmailAddress, Password);
+                }
+
+                // We got this far, this must be good
+                CanSave = true;
+                checkButton.IsEnabled = false;
+
+            } catch {
+                // Something failed to validate, so we can't save yet
+                CanSave = false;
+            }
+        }
+
+        private void checkButton_Click(object sender, RoutedEventArgs e) {
+            Validate();
+        }
+
+        // The subject and from text fields have no callbacks for a reason - it doesn't matter what value they have
+        // They're always valid!
+
+        private void emailAddress_TextChanged(object sender, TextChangedEventArgs e) {
+            Unvalidate();
+            if (emailAddress.Text.Contains("@gmail.com") || emailAddress.Text.Contains("@googlemail.com")) {
+                if (string.IsNullOrEmpty(hostname.Text)) {
+                    hostname.Text = "pop.gmail.com";
+                }
+            } else if (emailAddress.Text.Contains("@hotmail.com") || emailAddress.Text.Contains("@live.com")) {
+                if (string.IsNullOrEmpty(hostname.Text)) {
+                    hostname.Text = "pop3.live.com";
+                }
+            }
+        }
+
+        private void password_PasswordChanged(object sender, RoutedEventArgs e) {
+            Unvalidate();
+        }
+
+        private void hostname_TextChanged(object sender, TextChangedEventArgs e) {
+            Unvalidate();
+            // Fill in some defaults, just to be nice :)
+            if (hostname.Text == "pop.gmail.com") {
+                if (string.IsNullOrEmpty(port.Text)) {
+                    port.Text = "995";
+                    useSsl.IsChecked = true;
+                }
+            } else if (hostname.Text == "pop3.live.com") {
+                if (string.IsNullOrEmpty(port.Text)) {
+                    port.Text = "995";
+                    useSsl.IsChecked = true;
+                }
+            }
+        }
+
+        private void port_TextChanged(object sender, TextChangedEventArgs e) {
+            port.Text = Regex.Replace(port.Text, "[^0-9]", "", RegexOptions.Compiled);
+            Unvalidate();
+        }
+
+        private void useSsl_Checked(object sender, RoutedEventArgs e) {
+            Unvalidate();
+        }
+
+        private void useSsl_Unchecked(object sender, RoutedEventArgs e) {
+            Unvalidate();
+        }
+
+        private void subject_TextChanged(object sender, TextChangedEventArgs e) {
+            Subject = subject.Text;
+        }
+
+        private void from_TextChanged(object sender, TextChangedEventArgs e) {
+            From = from.Text;
+        }
+    }
+}
